19b6138a40463e28f58a031fee5f324de51b7238
TTHoque <115659002+TTHoque@users.noreply.github.com>
2023-02-06 21:00:33 +0000

    done

--- Week02/Lab_Week02/wb_lists.cpp
+++ Week02/Lab_Week02/wb_lists.cpp
@@ -7,19 +7,41 @@ using namespace std;
 void fisher_yates_shuffle(int* array, int length)
 {
     int k, temp, last;
-    for (int i=0; i<length; i++) {
+    for (int i=0; i<length-1; i++) {
         last = length-i;
         k = rand() % (last); 
-        cout << "k: " << k << endl;
         temp = array[k];
         array[k] = array[last-1];
         array[last-1] = temp;
     }
 }
 
-int generator(int n)
+int counter(int n) 
 {
-    int * array = new int[10];
+    /* 
+    This function will serve multiple purposes:
+    - generate an array of 1s and -1s of length 2n
+    - count how many iterations of fisher-shuffle are necessary for a well balanced list 
+    - deallocate memory for the array
+    */
+    if (n<=0) return 0;
+    int * array = generator(n);
+    int count = 0;
+    do {
+        count++;
+        fisher_yates_shuffle(array, 2*n);
+    } while ((!non_negative_sum(array, 2*n)) && (!non_positive_sum(array, 2*n)));
+
+    delete[] array;
+
+    return count;
+}
+
+int* generator(int n)
+{
+    if (n<=0) return NULL;
+
+    int * array = new int[2*n];
     for (int i=0; i<n; i++) {
         array[i] = 1;
     }
@@ -27,11 +49,5 @@ int generator(int n)
         array[j] = -1;
     }
 
-    int count = 0;
-    do {
-        count++;
-        fisher_yates_shuffle(array, 2*n);
-    } while (non_negative_sum(array, 2*n) || non_positive_sum(array, 2*n));
-
-    return count;
+    return array;
 }
