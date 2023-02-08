04945c9c7c25fc55212ba1891603bb69c141295e
emunix <httahera@gmail.com>
2023-02-08 11:03:51 -0500

    done with exercise

---
+++ Week03/Lab_Week03/balanced_lists.h
@@ -0,0 +1,7 @@
+#ifndef __BALANCED_LISTS_H
+#define __BALANCED_LISTS_H
+
+int lowest_depth(int array[], int length);
+int* new_balanced_list(int array[], int length);
+
+#endif
---
+++ Week03/Lab_Week03/unit_test_bl.cpp
@@ -0,0 +1,10 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "doctest.h"
+
+
+TEST_CASE("Testing neg_prefix_min function") 
+{
+    CHECK(neg_prefix(min(nums_0, sizeof(nums_0)/sizeof(nums_0[0])) == )
+
+    int expected_list_3[] = {1, 1, -1, -1, 1};
+}
