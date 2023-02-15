863d7cb3337d380b9219afdcc69919d0f022c378
TTHoque <115659002+TTHoque@users.noreply.github.com>
2023-02-15 20:31:54 +0000

    some work

--- Week04/STL/stack.h
+++ Week04/STL/stack.h
@@ -6,5 +6,8 @@
 using namespace std;
 
 stack<int> get_stack(const int n);
+stack<int> push_element(const stack<int> stack, int v);
+stack<int> pop_element(const stack<int> stack);
+stack<int> read_file()
 
 #endif __STACK_H
