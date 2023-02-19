fcc0a129946b32185c3eff6b6cb6aeaf262ebec6
emunix <httahera@gmail.com>
2023-02-19 01:11:25 -0500

    queue setup/\

--- Week04/STL/queue.h
+++ Week04/STL/queue.h
@@ -11,7 +11,7 @@ queue<int> get_int_queue(const int n);
 queue<int> int_queue_from_file(string file);
 int pop_and_print_queue(queue<int>);
 
-queue<struct node> get_queue_stack(const int n);
+queue<struct node> get_node_queue(const int n);
 queue<struct node> queue_stack_from_file(string file);
 int pop_and_print_nodes(queue<struct node> stack);
 
