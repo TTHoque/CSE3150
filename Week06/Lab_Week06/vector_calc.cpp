beec40903f298a0c39f098a6c45851b337eeb8e3
emunixog <httahera@gmail.com>
2023-03-03 23:47:34 -0500

    done with lab week 6

--- Week06/Lab_Week06/vector_calc.cpp
+++ Week06/Lab_Week06/vector_calc.cpp
@@ -5,6 +5,8 @@
 #include <exception>
 #include <cmath>
 
+using namespace std;
+
 vector<int_vector> vectors_from_file(string filename)
 {
     vector<int_vector> vect;
@@ -14,10 +16,10 @@ vector<int_vector> vectors_from_file(string filename)
         if (!input) throw exception();
         string x, y;
         int_vector v;
-        if (!(input >> x >> y)) throw exception();
+        if (!(input >> x >> y)) return vect;
         do {
-            v.x= atoi(x.c_str());
-            v.y = atoi(y.c_str());
+            v.x= atof(x.c_str());
+            v.y = atof(y.c_str());
             v.magnitude = v.magnitude_calc();
 
             vect.push_back(v);
@@ -31,32 +33,77 @@ vector<int_vector> vectors_from_file(string filename)
     return vect;
 }
 
-int int_vector::magnitude_calc()
+double int_vector::magnitude_calc()
 {  
     return sqrt(pow(x, 2) + pow(y, 2));
 }
 
-int vector_pair::dot_product()
+double vector_pair::dot_product()
 {
-
+    return v1.x * v2.x + v1.y * v2.y;
 }
 
-int vector_pair::cosign_angle()
+double vector_pair::angle_calc()
 {
+    if (v1.magnitude == -1) v1.magnitude = v1.magnitude_calc();
+    if (v2.magnitude == -1) v2.magnitude = v2.magnitude_calc();
 
+    if (v1.x == v2.x && v1.y == v2.y) return 0; // angle of overlapping vectors is 0
+    return acos(dot_product() / (v1.magnitude * v2.magnitude));
 }
 
-void sort_vector_of_vp_bv2_cosine_closeness(const vector<vector_pair> & vect) 
+vector<vector_pair> sort_vector_pairs_by_cosine_closeness(const vector<int_vector> & vect) 
 {
-    
+    vector<vector_pair> vect_vp;
+    vector_pair vp;
+    for (auto v1: vect) {
+        for (auto v2: vect) {
+            vp.v1 = v1;
+            vp.v2 = v2;
+            vp.angle = vp.angle_calc();
+            vect_vp.push_back(vp);
+        }
+    }
+
+    sort(vect_vp.begin(),
+         vect_vp.end(),
+         [](const vector_pair & left, const vector_pair & right)
+         {
+            return cos(left.angle) < cos(right.angle);
+         }
+    );
+
+    return vect_vp;
 }
 
-void print_vector_pairs(const vector<vector_pair> & vect)
+void print_vectors_from_vector(const vector<int_vector> & vect)
 {
-    for (auto vp: vect) {
-        cout << "magnitude: " << vp.magnitude() 
-             << " v1.to: " << vp.v1.to 
-             << " v2.to: " << vp.v2.to 
+    for (auto v: vect) {
+        cout << "v.x: " << v.x 
+             << ",   v.y: " << v.y  
+             << ",   v.magnitude: " << v.magnitude 
              << endl;
     }
+}
+
+void print_int_vector(const int_vector & iv) 
+{
+    cout << "   int_vector: "
+         << "x = " << iv.x << ", y = " << iv.y 
+         << ", magnitude: " << iv.magnitude << endl;
+}
+
+void print_vector_pair(const vector_pair & vp)
+{
+    cout << "vector_pair: " << endl;
+    print_int_vector(vp.v1);
+    print_int_vector(vp.v2);
+    cout << "   angle between vectors: " << vp.angle << endl;
+}
+
+void print_vector_pairs_from_vector(const vector<vector_pair> & vect)
+{
+    for (auto vp: vect) {
+        print_vector_pair(vp);
+    }   
 }
--- Week06/Lab_Week06/vector_calc.h
+++ Week06/Lab_Week06/vector_calc.h
@@ -6,26 +6,29 @@
 using namespace std;
 
 struct int_vector {
-    int x; // from represents the origin, from = 0
-    int y;
-    int magnitude;
+    double x; // from represents the origin, from = 0
+    double y;
+    double magnitude;
     int_vector(const int_vector & i_v) : x(i_v.x), y(i_v.y) , magnitude(i_v.magnitude){}; 
-    int_vector() : x(0), y(0), magnitude(0){}; 
-    int magnitude_calc();
+    int_vector() : x(0), y(0), magnitude(-1){}; 
+    double magnitude_calc();
 };
 
 struct vector_pair {
     int_vector v1;
     int_vector v2;
-    vector_pair(const vector_pair & vp) : v1(vp.v1), v2(vp.v2) {};
-    vector_pair(): v1(int_vector()), v2(int_vector()) {};
-    int dot_product();
-    int cosign_angle();
+    double angle;
+    vector_pair(const vector_pair & vp) : v1(vp.v1), v2(vp.v2), angle(vp.angle){};
+    vector_pair(): v1(int_vector()), v2(int_vector()), angle(0.0){};
+    double dot_product();
+    double angle_calc();
 };
 
 vector<int_vector> vectors_from_file(string filename);
-void sort_vector_of_vp_by_cosine_closeness(const vector<vector_pair> & vect);
-void print_vector_pairs(const vector<vector_pair> & vect);
-
+vector<vector_pair> sort_vector_pairs_by_cosine_closeness(const vector<int_vector> & vect);
+void print_vectors_from_vector(const vector<int_vector> & vect);
+void print_int_vector(const int_vector & iv);
+void print_vector_pair(const vector_pair & vp);
+void print_vector_pairs_from_vector(const vector<vector_pair> & vect);
 
 #endif
